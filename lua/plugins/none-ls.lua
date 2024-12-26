311499ba6c2c6dda49f205327eff00e8065e38e5
badr-saud <alrhbyb387@gmail.com>
2024-12-26 15:03:57 +0400

    updated: LSP configurations

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,24 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.latexindent,
-				null_ls.builtins.formatting.gofumpt,
-				null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.shfmt,
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.diagnostics.ltex,
-				null_ls.builtins.diagnostics.gopls,
-				null_ls.builtins.diagnostics.shellcheck,
-			},
-		})
-		vim.keymap.set("n", "<space>gf", vim.lsp.buf.format, {})
-	end,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.latexindent,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.ltex,
+        null_ls.builtins.diagnostics.gopls,
+        null_ls.builtins.diagnostics.shellcheck,
+      },
+    })
+    vim.keymap.set("n", "<space>gf", vim.lsp.buf.format, {})
+  end,
 }
