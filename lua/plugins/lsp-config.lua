ab7cc17a2bec1a9c624ad6c55774703221c5c3e0
badr-saud <alrhbyb387@gmail.com>
2025-08-24 17:00:42 +0400

    added: markdwon-preview, live-server, emmet-lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,152 +1,156 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"pyright",
-					"eslint",
-					"texlab",
-					"tsserver",
-					"ltex",
-					"gopls",
-					"clangd",
-					"cssls",
-					"bashls",
-					"typescript-language-server",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "eslint",
+          "texlab",
+          "tsserver",
+          "ltex",
+          "gopls",
+          "clangd",
+          "cssls",
+          "bashls",
+          "typescript-language-server",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			-- Lua
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.emmet_language_server.setup({
+        capabilities = capabilities,
+      })
 
-			-- TypeScript
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+      -- Lua
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			-- Python
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+      -- TypeScript
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
 
-			-- JavaScript
-			lspconfig.eslint.setup({
-				capabilities = capabilities,
-			})
+      -- Python
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
 
-			-- c++
-			lspconfig.clangd.setup({
-				on_attached = function(client, bufnr)
-					client.server_capabilities.signatureHelpProvider = false
-				end,
-				capabilities = capabilities,
-			})
+      -- JavaScript
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+      })
 
-			-- go lang
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-				cmd = { "gopls" },
-				file_types = { "go", "gomod", "gowork", "gotmpl" },
-				settings = {
-					gopls = {
-						completeUnimported = true,
-						usePlaceholders = true,
-						analysis = {
-							unusedparams = true,
-						},
-					},
-				},
-			})
+      -- c++
+      lspconfig.clangd.setup({
+        on_attached = function(client, bufnr)
+          client.server_capabilities.signatureHelpProvider = false
+        end,
+        capabilities = capabilities,
+      })
 
-			-- LaTeX
-			lspconfig.texlab.setup({
-				capabilities = capabilities,
-				settings = {
-					texlab = {
-						build = {
-							onSave = true, -- Automatically build on save
-							executable = "latexmk",
-							args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-						},
-						forwardSearch = {
-							executable = "zathura", -- Replace with your PDF viewer
-							args = { "--synctex-forward", "%l:1:%f", "%p" },
-						},
-						chktex = {
-							onOpenAndSave = true,
-							onEdit = true,
-						},
-					},
-				},
-			})
+      -- go lang
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        cmd = { "gopls" },
+        file_types = { "go", "gomod", "gowork", "gotmpl" },
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analysis = {
+              unusedparams = true,
+            },
+          },
+        },
+      })
 
-			-- LaTeX with ltex for grammar checking
-			lspconfig.ltex.setup({
-				capabilities = capabilities,
-				settings = {
-					ltex = {
-						language = "en", -- Set your preferred language
-					},
-				},
-			})
+      -- LaTeX
+      lspconfig.texlab.setup({
+        capabilities = capabilities,
+        settings = {
+          texlab = {
+            build = {
+              onSave = true, -- Automatically build on save
+              executable = "latexmk",
+              args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+            },
+            forwardSearch = {
+              executable = "zathura", -- Replace with your PDF viewer
+              args = { "--synctex-forward", "%l:1:%f", "%p" },
+            },
+            chktex = {
+              onOpenAndSave = true,
+              onEdit = true,
+            },
+          },
+        },
+      })
 
-			-- HTML with Jinja support
-			lspconfig.html.setup({
-				capabilities = capabilities,
-				filetypes = { "html", "jinja", "jinja.html" }, -- Add Jinja filetypes
-				settings = {
-					html = {
-						format = {
-							templating = true, -- Enable templating for Jinja
-						},
-					},
-				},
-			})
+      -- LaTeX with ltex for grammar checking
+      lspconfig.ltex.setup({
+        capabilities = capabilities,
+        settings = {
+          ltex = {
+            language = "en", -- Set your preferred language
+          },
+        },
+      })
 
-			-- CSS/SCSS support
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-				filetypes = { "css", "scss", "less" }, -- Add SCSS and Less
-				settings = {
-					css = {
-						validate = true,
-					},
-					scss = {
-						validate = true,
-					},
-					less = {
-						validate = true,
-					},
-				},
-			})
+      -- HTML with Jinja support
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        filetypes = { "html", "jinja", "jinja.html" }, -- Add Jinja filetypes
+        settings = {
+          html = {
+            format = {
+              templating = true, -- Enable templating for Jinja
+            },
+          },
+        },
+      })
 
-			-- Shell scripting (Bash)
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-				filetypes = { "sh", "bash", "zsh" }, -- Shell file types
-			})
+      -- CSS/SCSS support
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+        filetypes = { "css", "scss", "less" }, -- Add SCSS and Less
+        settings = {
+          css = {
+            validate = true,
+          },
+          scss = {
+            validate = true,
+          },
+          less = {
+            validate = true,
+          },
+        },
+      })
 
-			-- Keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      -- Shell scripting (Bash)
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+        filetypes = { "sh", "bash", "zsh" }, -- Shell file types
+      })
+
+      -- Keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
