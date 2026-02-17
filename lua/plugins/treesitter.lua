7f93c0ff08dfa85618bb754553ea5fe57c42fd41
badr-saud <alrhbyb387@gmail.com>
2026-02-17 12:01:24 +0400

    treesitter fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
+		require "nvim-treesitter".setup({
 			auto_install = true,
 			highlight = { enable = true, disable = {"latex"} },
 			indent = { enable = true, disable={"latex"} },
